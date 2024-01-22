55d40436144bc0f38d000fd1e13f1c5a771a8074
Dimas Wahyu Pratomo <dwp1725@gmail.com>
2024-01-22 21:06:51 +0700

    Berhasil handling ambigous data SQL

--- app/Http/Controllers/Backend/GugusDepan/PengujiGudepController.php
+++ app/Http/Controllers/Backend/GugusDepan/PengujiGudepController.php
@@ -6,6 +6,7 @@ use DataTables;
 use App\Http\Controllers\Controller;
 use App\Models\AccountPengujiGudep;
 use App\Models\PengurusGudep;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
@@ -20,41 +21,47 @@ class PengujiGudepController extends Controller
         //
         if ($request->ajax()) {
 
-            $data = AccountPengujiGudep::leftjoin('pengurus_gudeps', 'account_penguji_gudeps.gudep_id', '=', 'pengurus_gudeps.id')->select('account_penguji_gudeps.id', 'pengurus_gudeps.name', 'account_penguji_gudeps.email', 'account_penguji_gudeps.created_at')->latest()->get();
+            $data = AccountPengujiGudep::where('account_penguji_gudeps.gudep_id', Auth::user()->id_gudep)
+                ->leftJoin('pengurus_gudeps', 'account_penguji_gudeps.pengurus_id', '=', 'pengurus_gudeps.id')
+                ->select('account_penguji_gudeps.id', 'pengurus_gudeps.name', 'account_penguji_gudeps.email', 'account_penguji_gudeps.role_id', 'account_penguji_gudeps.created_at')
+                ->latest()
+                ->get();
+
+            // dd($data);
 
             // Belum Selesai untuk pemanggilan Join Data
 
             return datatables::of($data)
-                    ->addIndexColumn()
-                    ->filter(function ($instance) use ($request) {
-                        if (!empty($request->get('pengurus_id'))) {
-                            $instance->collection = $instance->collection->filter(function ($row) use ($request) {
-                                return Str::contains($row['pengurus_id'], $request->get('pengurus_id')) ? true : false;
-                            });
-                        }
+                ->addIndexColumn()
+                ->filter(function ($instance) use ($request) {
+                    if (!empty($request->get('email'))) {
+                        $instance->collection = $instance->collection->filter(function ($row) use ($request) {
+                            return Str::contains($row['email'], $request->get('email')) ? true : false;
+                        });
+                    }
 
-                        if (!empty($request->get('search'))) {
-                            $instance->collection = $instance->collection->filter(function ($row) use ($request) {
-                                if (Str::contains(Str::lower($row['pengurus_id']), Str::lower($request->get('search')))){
-                                    return true;
-                                }
-                                return false;
-                            });
-                        }
-                    })
-                    ->addColumn('action', function($row){
-                        $dropBtn ='<div class="dropdown">
+                    if (!empty($request->get('search'))) {
+                        $instance->collection = $instance->collection->filter(function ($row) use ($request) {
+                            if (Str::contains(Str::lower($row['email']), Str::lower($request->get('search')))) {
+                                return true;
+                            }
+                            return false;
+                        });
+                    }
+                })
+                ->addColumn('action', function ($row) {
+                    $dropBtn = '<div class="dropdown">
                         <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
                         <div class="dropdown-menu">
-                          <a class="dropdown-item" href='.route("penguji-gudeps.edit", $row->id).'><i class="bx bx-edit-alt me-1"></i> Ubah</a>
+                          <a class="dropdown-item" href=' . route("penguji-gudeps.edit", $row->id) . '><i class="bx bx-edit-alt me-1"></i> Ubah</a>
                           <form action="' . route('penguji-gudeps.destroy', $row->id) . '" method="POST">' . csrf_field() . method_field("DELETE") . '<button type="submit" class="btn btn-light" onclick="return confirm(\'Beneran nih mau di hapus ?\')"><i class="bx bx-trash me-1"></i> Hapus</button></form>
                         </div>
                         </div>';
-                        $btn = $dropBtn;
-                        return $btn;
-                    })
-                    ->rawColumns(['action'])
-                    ->make(true);
+                    $btn = $dropBtn;
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
         }
 
         return view('backend.gugusdepan.penguji_gudep.list');
@@ -76,7 +83,7 @@ class PengujiGudepController extends Controller
     public function store(Request $request)
     {
         //
-        $this->validate($request,[
+        $this->validate($request, [
             'gudep_id'     => 'required',
             'admin_gudep_id'     => 'required',
             'pengurus_id'     => 'required',
@@ -94,10 +101,10 @@ class PengujiGudepController extends Controller
             'role_id'     => $request->role_id
         ]);
 
-        if($pengujigudeps){
+        if ($pengujigudeps) {
             //redirect dengan pesan sukses
             return redirect()->route('penguji-gudeps.index')->with(['success' => 'Data Berhasil Disimpan!']);
-        }else{
+        } else {
             //redirect dengan pesan error
             return redirect()->route('penguji-gudeps.add')->with(['error' => 'Data Gagal Disimpan!']);
         }
